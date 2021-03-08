1410ebf223f26c612e890dab42bfa53c14c21a10
brunahelenas <shbhelena@gmail.com>
2021-03-08 15:18:12 -0300

    Atualização com inner class e private set

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,6 +4,13 @@ class Pessoa {
 
     var nome: String = "Bruna"
     var cpf: String = "123.785.014.87"
+    private set
+
+    inner class Endereco{
+
+        var rua: String = "Aristides Cury"
+    }
+
 }
 
 fun main(){
@@ -11,5 +18,5 @@ fun main(){
     val bruna = Pessoa()
     println(bruna.nome)
     println(bruna.cpf)
-
+    println(bruna.Endereco().rua)
 }

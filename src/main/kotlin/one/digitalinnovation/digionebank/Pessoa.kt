89dfb99031c7453fbc04e2345d7499f55eebfe70
brunahelenas <shbhelena@gmail.com>
2021-03-08 16:06:45 -0300

    Constructor and function Person

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -6,17 +6,13 @@ class Pessoa {
     var cpf: String = "123.785.014.87"
     private set
 
-    inner class Endereco{
+    constructor()
 
-        var rua: String = "Aristides Cury"
-    }
-
+    fun UnePessoa() = "$nome e $cpf"
 }
 
 fun main(){
 
     val bruna = Pessoa()
-    println(bruna.nome)
-    println(bruna.cpf)
-    println(bruna.Endereco().rua)
+    println(bruna.UnePessoa())
 }

3642705e6b1926c33a2d8be3254f1c0e16f73f26
brunahelenas <shbhelena@gmail.com>
2021-03-10 15:34:58 -0300

    Project Finish

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -4,7 +4,9 @@ import one.digitalinnovation.digionebank.Gerente
 
 fun main(){
 
-    val maria = Gerente("Maria","1020304050", 5000.0)
+    val maria = Gerente("Maria","1020304050", 5000.0, "Senha 123")
     ImprimeRelatorioFuncionario.imprime(maria)
+
+    TesteAutenticacao().autentica(maria)
 }
 
